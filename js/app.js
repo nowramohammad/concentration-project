ec87e3d71125c195990f384b3ec4d5a786df42cc
User Name <nowramohammad@Nowras-MacBook-Pro.local>
2024-08-22 08:28:52 -0500

    adding files

--- js/app.js
+++ js/app.js
@@ -10,4 +10,60 @@ let matchedPairs = 0;
 let remainingAttempts = 3;
 let timeleft = 30;
 let timer;
+function startGame () {
+    shuffledImages.forEach(image => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.image = image;
 
+        const imgElemnt = document.createElement('img');
+        imgElement.src = `images/${image}`;
+        card.appendChild(imgElemnt);
+        card.addEventListener("click", flipCard);
+        gameContainer.appendChild(card);
+
+     });
+    timer = setInterval(() => {
+        
+        timerLeftElement.innerText = timeLeft;
+        if (timeLeft === 0) {
+            endGame(fales);
+        }
+    }, 1000);
+    }
+ function flipCard() {
+    if (flippedCards.length < 2 && !this.classList.contains("flipped")) {
+        this.classList.add('flipped');
+        flippedCards.push(this);
+        if (flippedCards.length === 2) {
+            checkForMatch();
+        }
+    }
+ }
+ 
+ function checkForMatch() {
+    const [card1, card2] = flippedCards;
+    if (card1.dataset.omage === card2.dataset.image) {
+        matchedPairs++;
+        flippedCards =[];
+        if (matchPairs === images.length) {
+            endGame(true);
+        }
+    } else {
+        remaingAttemptsElement.innerText = remaingAttempts;
+        setTimeout(() => {
+            card1.classList.remove("flipped");
+            card2.classList.remove("flipped");
+            flippedCards = [];
+        }, 1000);
+        if (remainingAttempts === 0) {
+            endGame(false);
+        }
+    }
+ }
+  function endGame(won) {
+    clearInterval(timer);
+    messageElement.innerText = won? "YOU WIN" : "YOU LOSE!"; 
+    document.querySelectorAll(".card").forEach(card => card.removeEventListener("click", flipCard));
+  }
+ startGame(); 
